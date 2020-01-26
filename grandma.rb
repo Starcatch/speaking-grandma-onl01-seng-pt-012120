5c27a360f6631b1b47cd5558cd0614fc2a72b43b
Timur Mishiev <timurmishiev@gmail.com>
2020-01-26 02:19:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 
 def speak_to_grandma(phrase)
  if phrase == "I LOVE YOU GRANDMA!"
- puts "I LOVE YOU TOO PUMPKIN!"
+  "I LOVE YOU TOO PUMPKIN!"
  elsif phrase.upcase()
  "HUGH?! SPEAK UP, SUNNY!"
  else
