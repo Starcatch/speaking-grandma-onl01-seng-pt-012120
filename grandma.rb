908a9ab42a10154bab1820b000d7728545c3ce4c
Timur Mishiev <timurmishiev@gmail.com>
2020-01-26 02:53:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,13 @@
+
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN"
-  elsif phrase ≠ prase.upcase()
-    "HUH?! SPEAK UP, SONNY!"
-  else 
+ if phrase =="I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+ elsif == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
-  end 
+ elsif phrase == "WHAT?"
+    "NO, NOT SINCE 1938!"
+  else 
+    "HUH?! SPEAK UP, SONNY!"
+ end
 end 
   
