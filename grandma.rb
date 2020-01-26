72c15d35b7830b1503427450e1414679e3c81a68
Timur Mishiev <timurmishiev@gmail.com>
2020-01-26 02:27:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 
 def speak_to_grandma(phrase)
  if phrase == "I LOVE YOU GRANDMA!"
-  "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
  elsif phrase == "WHAT DID YOU EAT TODAY?" || "WHAT?"
- "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
  else
    "HUGH?! SPEAK UP, SUNNY!"
  end
